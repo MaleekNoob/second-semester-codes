e95bfc0f12b155f95bceec39caf2cab1995c25eb
MaleekNoob <maleekhussainali456@gmail.com>
2023-03-12 11:28:33 +0500

    commit log: classes(destructor).cpp

--- classes(destructor).cpp
+++ classes(destructor).cpp
@@ -8,9 +8,56 @@ Program to clear conepts regarding destructors in c++
 #include <iostream>
 using namespace std;
 
+class Class {
+    private:
+        int int_data;
+        double double_data;
+        char char_data;
+        int* dynamic_int_data = new int;
+
+    public:
+    Class() {
+        int_data = 0;
+        double_data = 0;
+        char_data = 0;
+        *dynamic_int_data = 0;
+    }
+
+    Class(int integer, double decimal, char character, int* dynamic): int_data(integer), double_data(decimal), char_data(character) {
+        *dynamic_int_data = *dynamic; //deep copy
+    }
+
+    // Class(Class &obj) {
+    //     this->char_data = obj.char_data;
+    //     this->double_data = obj.double_data;
+    //     this->int_data = obj.int_data;
+    //     this->dynamic_int_data = 
+    // }
+};
+
 int main() {
 
+    int* dynamic = new int;
+    *dynamic = 10;
 
+    cout << endl << "ADDRESS AND VALUE OF DYNAMIC VARIABLE BEFORE MODIFICATION";
+    cout << endl << "Address: " << dynamic << endl << "Value: " << *dynamic;
+
+    int* dynamic_arr = new int[5]{1, 2, 3, 4, 5};
+    cout << endl;
+
+    cout << endl << "ADDRESS AND VALUE OF DYNAMIC ARRAY BEFORE MODIFICATION";
+    cout << endl << "Address: " << dynamic_arr << endl << "Values: ";
+    for (int i = 0; i < 5; i++) {
+        cout << endl << *(dynamic_arr + i);
+    }
+
+    *dynamic = *dynamic_arr;
+    cout << endl << "ADDRESS AND VALUE OF DYNAMIC VARIABLE AFTER MODIFICATION";
+    cout << endl << "Address: " << dynamic << endl << "Values: ";
+    for (int i = 0; i < 5; i++) {
+        cout << endl << *(dynamic + i);
+    }
 
     return 0;
 }
