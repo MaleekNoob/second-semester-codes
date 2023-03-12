325cba864e18f299bcf762aca6d6308958e17a58
MaleekNoob <maleekhussainali456@gmail.com>
2023-03-12 11:05:13 +0500

    commit log: classes(destructor) Testing of user defined destructor and dynamic memory allocation

---
+++ classes(destructor).cpp
@@ -0,0 +1,16 @@
+/*
+Program to clear conepts regarding destructors in c++
+1. See what happens to dynamic classes with default destructor
+2. See what happens to dynamic classes with our very own destructor
+3. Play around with dynamic data members within class via default and user-built destructor
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+
+    return 0;
+}
