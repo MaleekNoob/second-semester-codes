dc6c65cd2cc9e8aa53dd8e1643cec02ef81bfbb3
Maleek Hussain <i221526@nu.edu.pk>
2023-04-08 20:06:23 +0500

    aggregation and composition da

--- classes(dates).cpp
+++ classes(dates).cpp
@@ -1,119 +1,124 @@
 #include <iostream>
 using namespace std;
 
-class Date {
-    private:
-    int day;
-    int month;
-    int year;
+class Engine {
+    string engineNum;
 
     public:
-
-    Date() {
-        day = 0;
-        month = 0;
-        year = 0;
+    Engine(string engineNum = "") {
+        this->engineNum = engineNum;
     }
 
-    Date(int d, int m, int y):day(d), month(m), year(y) {}
-
-    void addDay(int d) {
-        day = day + d;
-
-        while (day > 31) {
-            day -= 31;
-            month++;
-
-            while (month > 12) {
-                month -= 12;
-                year++;
-            }
-        }
+    string getEngineNum() {
+        return engineNum;
     }
 
-    void addWeek(int week) {
-        day += week * 7;
-
-        while (day > 31) {
-            day -= 31;
-            month++;
-
-            while (month > 12) {
-                month -= 12;
-                year++;
-            }
-        }
-    }
-
-    void addMonth(int m) {
-        month += m;
-
-        while (month > 12) {
-            month -= 12;
-            year++;
-        }
-    }
-
-    void Add(int d, int m) {
-        addDay(d);
-        addMonth(m);
-    }
-
-    bool isLeapYear() {
-        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
-            return true;
-        }
-        else {return false;}
-    }
-
-    int getDay() {
-        return day;
-    }
-
-    int getMonth() {
-        return month;
-    }
-
-    int getYear() {
-        return year;
-    }
-
-    void setDay(int d) {
-        day = d;
-    }
-
-    void setMonth(int m) {
-        month = m;
-    }
-
-    void setYear(int y) {
-        year = y;
+    void setEngineNum(string engineNum) {
+        this->engineNum = engineNum;
     }
 
     void display() {
-        cout << day << "/" << month << "/" << year << endl;
+        cout << "Engine number: " << engineNum << endl;
+    }
+};
+
+class Car {
+    string make;
+    double price;
+    Engine engine;  // Composition
+
+    public:
+    Car(string make = "", double price = 0.0, Engine engine = {""}) {
+        this->make = make;
+        this->price = price;
+        this->engine = engine;
     }
 
+    string getMake() {
+        return make;
+    }
+
+    double getPrice() {
+        return price;
+    }
+
+    Engine getEngine() {
+        return engine;
+    }
+
+    void setMake(string make) {
+        this->make = make;
+    }
+
+    void setPrice(double price) {
+        this->price = price;
+    }
+
+    void setEngine(Engine engine) {
+        this->engine = engine;
+    }
+
+    void display() {
+        cout << "Make: " << make << ", Price: " << price << ", Engine ID: " << engine.getEngineNum() << endl;
+    }
+};
+
+class Workshop {
+    int numberOfVehicles, crew;
+    Engine* engine;  // Aggregation
+
+    public:
+    Workshop(int numberOfVehicles = 0, int crew = 0, Engine* engine = nullptr) {
+        this->numberOfVehicles = numberOfVehicles;
+        this->crew = crew;
+        this->engine = engine;
+    }
+
+    int getNumberOfVehicles() {
+        return numberOfVehicles;
+    }
+
+    int getCrew() {
+        return crew;
+    }
+
+    Engine* getEngine() {
+        return engine;
+    }
+
+    void setNumberOfVehicles(int numberOfVehicles) {
+        this->numberOfVehicles = numberOfVehicles;
+    }
+
+    void setCrew(int crew) {
+        this->crew = crew;
+    }
+
+    void setEngine(Engine* engine) {
+        this->engine = engine;
+    }
+
+    void display() {
+        cout << "Number of vehicles: " << numberOfVehicles << ", Crew: " << crew << ", Engine ID: " << engine->getEngineNum() << endl;
+    }
 };
 
 int main() {
 
-    int day, month, year;
-    Date d1;
-    cout << "\nEnter day: ";
-    cin >> day;
-    cout << "\nEnter month: ";
-    cin >> month;
-    cout << "\nEnter year: ";
-    cin >> year;
-    d1 = Date(day, month, year);
-    d1.Add(10, 2);
-    d1.addDay(10);
-    d1.addWeek(2);
-    d1.addMonth(2);
-    cout << endl << d1.isLeapYear() << endl;
+    Engine engine1("1234567890");
+    Car car1("Ford", 10000.00, engine1);
+    engine1 = Engine("0987654321");
+    Car car2("Toyota", 20000.00, engine1);
+    cout << "Displaying cars:" << endl;
+    car1.display();
+    car2.display();
 
-    d1.display();
+    Workshop workshop1(10, 5, &engine1);
+    engine1 = Engine("4587881209");
+    Workshop workshop2(20, 10, &engine1);
+    cout << "Displaying workshops:" << endl;
+    workshop1.display();
+    workshop2.display();
 
 
     return 0;
